066f76ba6a78a6e5be28e1376a49baccc719c013
fsanchez2334 <fsanchez@cristoreyny.org>
2017-11-01 22:16:50 +0000

    Added more javascript

--- js/script.js
+++ js/script.js
@@ -28,3 +28,32 @@ secondName.on('value',function(snapshot){
 });
 
 var containfo=firebase.database().ref()
+
+var projects=firebase.database().ref('projects');
+
+projects.on('val',function(snapshot){
+    $(".testing-space").empty();
+    var projects=snapshot.val()
+    
+    var div=$('<div class="col-sm-4 portfolio-item">');
+    var thumbnail= $('<div class="thumbnail">');
+    var title=$('<h4 class="text-center" id="project1-title">');
+    title.text(projects[key].project.title);
+    var img= $('<img src="" class="img-responsive">');
+    img.attr("src", projects[key]l.project_image_url);
+    var caption= $('<div class="caption text-center">');
+    var livesiteButton = $('<a class="btn btn-outline" href="">');
+    livesiteButton.attr("href", projects[key].live_site_url);
+    livesiteButton.text("Live Site");
+    var sourceButton= $('<a class="btn btn-outline" href=""> Source </a>');
+    sourceButton.attr("href", projects[key].source_url);
+    
+    caption.append(livesiteButton);
+    caption.append(sourceButton);
+    thumbnail.append(caption);
+    thumbnail.append(title);
+    thumbnail.append(img);
+    div.append(thumbnail);
+    
+    $
+}
